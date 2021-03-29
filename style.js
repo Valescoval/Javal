2f1f68b63f0cab4bb54d4fe647d6df963bc2e24f
Valescoval <81016196+Valescoval@users.noreply.github.com>
2021-03-29 05:20:55 +0400

    Add files via upload

---
+++ style.js
@@ -0,0 +1,12 @@
+
+
+function getStarted() {
+  alert("Hello Customer!");
+  const CustomerInterest = confirm('Would You Like a Taste of Our Classic Dishes?');
+  if (CustomerInterest == true) { alert('We Serve a Plate of Jollof Rice @ $1000');
+  
+} else{
+  alert('Okay, Let Me Get The Menu For You!');
+}
+  
+}
